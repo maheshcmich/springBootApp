fa558182f40ad4ee64d89129ad985edf47227fd3
Mahesh <maheshcmic@gmail.com>
2019-10-15 20:03:55 -0500

    vineela first git commit

--- src/main/java/edu/mahesh/spring/boot/Test.java
+++ src/main/java/edu/mahesh/spring/boot/Test.java
@@ -3,5 +3,9 @@ package edu.mahesh.spring.boot;
 public class Test {
     public static void main(String[] args) {
         System.out.println("Hello, git!");
+
+        System.out.println("hello vineela");
+
+        //comment
     }
 }
